6d49c2fb9d0282072f4cb1774c0c51182fb3d6b0
b1naryzer0 <binaryzero2@outlook.de>
2021-11-28 22:08:32 +0100

    Projektdateien hinzufügen.

---
+++ RadioPlayer/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace RadioPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.wmp = new AxWMPLib.AxWindowsMediaPlayer();
+            this.cbSender = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ni1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wmp
+            // 
+            this.wmp.Enabled = true;
+            this.wmp.Location = new System.Drawing.Point(12, 12);
+            this.wmp.Name = "wmp";
+            this.wmp.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp.OcxState")));
+            this.wmp.Size = new System.Drawing.Size(267, 96);
+            this.wmp.TabIndex = 0;
+            // 
+            // cbSender
+            // 
+            this.cbSender.BackColor = System.Drawing.Color.Black;
+            this.cbSender.ForeColor = System.Drawing.Color.White;
+            this.cbSender.Location = new System.Drawing.Point(12, 114);
+            this.cbSender.Name = "cbSender";
+            this.cbSender.Size = new System.Drawing.Size(267, 22);
+            this.cbSender.TabIndex = 1;
+            this.cbSender.SelectedIndexChanged += new System.EventHandler(this.cbSender_SelectedIndexChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Black;
+            this.btnExit.BackgroundImage = global::RadioPlayer.Properties.Resources.minmin;
+            this.btnExit.CausesValidation = false;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(202, 153);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(77, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Image = global::RadioPlayer.Properties.Resources.minmin;
+            this.label1.Location = new System.Drawing.Point(12, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 14);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fritz R. 11/28/2021";
+            // 
+            // ni1
+            // 
+            this.ni1.Text = "notifyIcon1";
+            this.ni1.Visible = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = global::RadioPlayer.Properties.Resources.minmin;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(291, 190);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.cbSender);
+            this.Controls.Add(this.wmp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bagel Radio Player 0.3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wmp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmp;
+        private System.Windows.Forms.ComboBox cbSender;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon ni1;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RadioPlayer/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace RadioPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[,] URLs =
+        {
+            { "Bagel 128k AAC", "https://ais-sa3.cdnstream1.com/2606_128.aac" },
+            { "Bagel 128k MP3", "https://ais-sa3.cdnstream1.com/2606_128.mp3" },
+            { "Bagel 64k AAC", "https://ais-sa3.cdnstream1.com/2606_64.aac" },
+            { "Bagel 32k AAC", "https://ais-sa3.cdnstream1.com/2606_32.aac" }
+        };
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbSender.Text = "Bagel 128k AAC";
+
+            for (int i = 0; i < URLs.Length / 2; i++)
+            {
+                cbSender.Items.Add(URLs[i, 0]);
+            }
+
+            // wmp.settings.mute = true;
+            wmp.settings.volume = 100;
+            wmp.URL = URLs[1, 1];
+            wmp.uiMode = "none"; // none, mini, full, invisible
+            
+            /*
+            if (wmp.currentMedia != null)
+            {
+                // Display the name of the current media item. 
+                // cbSender.Text = wmp.currentMedia.name; // KLAPPT NICHT
+
+                // DAS HIER KLAPPT, ABER LOGISCHERWEISE NUR GENAU EINMAL
+                // Task<string> task = GetMetaDataFromIceCastStream("https://ais-sa3.cdnstream1.com/2606_128.aac");
+            }
+            */
+        }
+
+        private void cbSender_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            wmp.URL = URLs[cbSender.SelectedIndex, 1];
+        }
+
+        /*
+        private void ni1_BalloonTipClicked(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        //Wenn das Fenster geschlossen wird, wird nachgefragt, ob der Player im Hintergrund laufen soll. 
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Soll das Programm im Hintergrund ausgeführt werden?", "Radioplayer", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                if (e.CloseReason == CloseReason.UserClosing)
+                {
+                    this.Hide();
+                    ni1.Visible = true;     //TrayIcon anzeigen 
+                    ni1.ShowBalloonTip(1, "Webradio", "Wiedergabe erfolgt im Hintergrund", ToolTipIcon.Info);
+                    e.Cancel = true;
+                }
+            }
+        }
+        */
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        public async Task<String> GetMetaDataFromIceCastStream(string iceCast)
+        {
+            HttpClient m_httpClient = new HttpClient();
+
+            m_httpClient.DefaultRequestHeaders.Add("Icy-MetaData", "1");
+            var response = await m_httpClient.GetAsync(iceCast, HttpCompletionOption.ResponseHeadersRead);
+            m_httpClient.DefaultRequestHeaders.Remove("Icy-MetaData");
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<string> headerValues;
+                if (response.Headers.TryGetValues("icy-metaint", out headerValues))
+                {
+                    string metaIntString = headerValues.First();
+                    if (!string.IsNullOrEmpty(metaIntString))
+                    {
+                        int metadataInterval = int.Parse(metaIntString);
+                        byte[] buffer = new byte[metadataInterval];
+                        using (var stream = await response.Content.ReadAsStreamAsync())
+                        {
+                            int numBytesRead = 0;
+                            int numBytesToRead = metadataInterval;
+                            do
+                            {
+                                int n = stream.Read(buffer, numBytesRead, 10);
+                                numBytesRead += n;
+                                numBytesToRead -= n;
+                            } while (numBytesToRead > 0);
+
+                            int lengthOfMetaData = stream.ReadByte();
+                            int metaBytesToRead = lengthOfMetaData * 16;
+                            byte[] metadataBytes = new byte[metaBytesToRead];
+                            var bytesRead = await stream.ReadAsync(metadataBytes, 0, metaBytesToRead);
+                            var metaDataString = System.Text.Encoding.UTF8.GetString(metadataBytes);
+
+                            string metaFormattedString = metaDataString.Replace("StreamTitle='", "");
+                            metaFormattedString = metaFormattedString.Replace("';", "");
+                            cbSender.Text = metaFormattedString;
+                            // string metaDataStringFormatted = metaDataString.Replace("StreamTitle='", "");
+                            // return metaDataString;
+                        }
+                    }
+                }
+            }
+            return "";
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Task<string> task = GetMetaDataFromIceCastStream(wmp.currentMedia.sourceURL);
+        }
+    }
+}
